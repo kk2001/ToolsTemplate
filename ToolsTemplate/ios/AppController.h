cd8711d2e0d3a1f4f11f079230ffcadc63546eb7
longlingxiu <longlingxiu@gmail.com>
2014-03-21 15:32:41 +0800

    first commits

---
+++ ToolsTemplate/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  ToolsTemplateAppController.h
+//  ToolsTemplate
+//
+//  Created by cesc on 14-3-21.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ ToolsTemplate/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ToolsTemplateAppController.h
+//  ToolsTemplate
+//
+//  Created by cesc on 14-3-21.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
